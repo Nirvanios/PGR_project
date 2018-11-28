858c0a1c5eade70b20c803a3f8b82f7672398403
xflajs00 <xflajs00@stud.fit.vutbr.cz>
2018-11-28 19:16:00 +0100

    add SimulatedModel

---
+++ simulation/SimulatedModel.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Petr on 28.11.2018.
+//
+
+#include "SimulatedModel.h"
---
+++ simulation/SimulatedModel.h
@@ -0,0 +1,14 @@
+//
+// Created by Petr on 28.11.2018.
+//
+
+#ifndef PGR_PROJECT_SIMULATEDMODEL_H
+#define PGR_PROJECT_SIMULATEDMODEL_H
+
+
+class SimulatedModel {
+
+};
+
+
+#endif //PGR_PROJECT_SIMULATEDMODEL_H
