f239e02cbd3f225ebddbae115a37cc3a384ccb3c
xflajs00 <xflajs00@stud.fit.vutbr.cz>
2018-11-28 19:24:10 +0100

    add ForceGenerator class

---
+++ simulation/ForceGenerator.h
@@ -0,0 +1,14 @@
+//
+// Created by Petr on 28.11.2018.
+//
+
+#ifndef PGR_PROJECT_FORCEGENERATOR_H
+#define PGR_PROJECT_FORCEGENERATOR_H
+
+
+class ForceGenerator {
+
+};
+
+
+#endif //PGR_PROJECT_FORCEGENERATOR_H
