a9c87f567fccf3f7c831cce8bff5897369c526ab
Petr <xflajs00@stud.fit.vutbr.cz>
2018-12-04 16:01:23 +0100

    rename shaders

--- graphics/core/Shader.h
+++ graphics/core/Shader.h
@@ -53,10 +53,10 @@ public:
         BindAttributeLocation(1, "inputColor");
         BindAttributeLocation(2, "inputNormal");
 
-        if (!LoadVertexShader("../tutorial2.vert"))
+        if (!LoadVertexShader("../main_vertex_shader.glsl"))
             return false;
 
-        if (!LoadFragmentShader("../tutorial3.frag"))
+        if (!LoadFragmentShader("../main_fragment_shader.glsl"))
             return false;
 
         // All shaders has been create, now we must put them together into one large object
