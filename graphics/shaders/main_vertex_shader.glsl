3b96eb28de9b6405a8b80a95c3322fed83852c15
Petr <xflajs00@stud.fit.vutbr.cz>
2018-12-04 18:54:32 +0100

    shader cleanup

--- graphics/shaders/main_vertex_shader.glsl
+++ graphics/shaders/main_vertex_shader.glsl
@@ -1,28 +1,10 @@
 #version 150
 
-/*// in_Position was bound to attribute index 0 and in_Color was bound to attribute index 1
-in vec3 in_Position;
-in vec4 in_Color;
-uniform mat4 translate;
-uniform mat4 mvp;
-// We output the ex_Color variable to the next shader in the chain
-out vec4 ex_Color;
-
-void main(void) {
-    // Since we are using flat lines, our input only had two points: x and y.
-    // Set the Z coordinate to 0 and W coordinate to 1
-    gl_Position = mvp * translate * vec4(in_Position, 1.0);
-
-    // Pass the color on to the fragment shader
-    ex_Color = in_Color;
-}*/
-
 in vec3 inputPosition;
 in vec4 inputColor;
 in vec3 inputNormal;
 
 uniform mat4 projection, modelview, normalMat;
-uniform mat4 translate;
 uniform vec3 lightPos;
 
 out vec4 forFragColor;
